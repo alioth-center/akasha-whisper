8c1f3eb49769123acef08ba8d4afacb5b710649d
sunist-c <sunist.c@outlook.com>
2024-09-12 15:06:14 +0800

    chore(linter): 忽略 embdings 的 linter 检查

--- frontend/embeddings.go
+++ frontend/embeddings.go
@@ -2,5 +2,9 @@ package frontend
 
 import "embed"
 
+// ManagementModule is the embedded frontend module for management.
+//
+// nolint
+//
 //go:embed management/dist/*
 var ManagementModule embed.FS
